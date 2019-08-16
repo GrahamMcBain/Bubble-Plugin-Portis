852b27d5ed7a597af1d9ed4a5bfc21d789cdca1d
Graham McBain <36456059+GrahamMcBain@users.noreply.github.com>
2019-08-16 11:13:43 -0700

    Automatic commit from Bubble

---
+++ actions/AAD/client.js
@@ -0,0 +1,13 @@
+function(properties, context) {
+
+
+
+  //Load any data 
+const p = ethers.getDefaultProvider()
+const portis = new Portis("211b48db-e8cc-4b68-82ad-bf781727ea9e", "mainnet");
+
+portis.showPortis()
+    
+
+
+}
